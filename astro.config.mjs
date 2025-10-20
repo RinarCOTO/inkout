7b0604d03e48edea9159b5518dc4fb8cd87a92f7
Rinar COTO <rinar@cotocollective.com>
2025-10-21 02:09:56 +0800

    Update astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,7 +5,7 @@ import tailwindcss from '@tailwindcss/vite';
 
 export default defineConfig({
   site: 'https://RinarCOTO.github.io',
-  base: '/inkOUT/',
+  base: '/inkout/#',
   integrations: [react()],
   vite: {
     plugins: [tailwindcss()],
